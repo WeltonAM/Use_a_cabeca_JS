79b1c2c9f9a12b6c3f86630cec4602b95e571724
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-05-10 23:43:22 -0300

    Bubble factory
    Refatorado e com solução do desafio

---
+++ Programação_JS/04_arrays-colocando-ordem-nos-códigos/02/bubbles.js
@@ -0,0 +1,56 @@
+let scores = [60, 50, 50, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 55, 61, 51, 44];
+
+let costs = [.25, .27, .25, .25, .25, .25, .33, .31, .25, .29, .27, .22, .31, .25, .25, .33, .21, .25, .25, .25, .28, .25, .24, .22, .20, .25, .30, .25, .24, .25, .25, .25, .27, .25, .26, .29];
+
+function printAndGetHighScore(scores){
+    let highScore = 0;
+    let output;
+    
+    for(let i = 0; i < scores.length; i++){
+        output = "Bubble solution #" + i + " score: " + scores[i];
+        console.log(output);
+        
+        if(scores[i] > highScore){
+            highScore = scores[i];
+        }
+    }
+    return highScore;
+}
+
+function getBestResults(scores, highScore) {
+    let bestSolutions = [];
+
+    for(let i = 0; i < scores.length; i++){
+        if(scores[i] == highScore){
+            bestSolutions.push(i);
+        }
+    }
+    return bestSolutions;
+}
+
+function getMostCostEffectiveSolution(costs){
+    let cost = 100;
+    let index;
+    let lowCost;
+
+    for(let i = 0; i < bestSolutions.length; i++){
+        index = bestSolutions[i];
+        
+        if(cost > costs[index]){
+            lowCost = index;
+            cost = costs[index];
+        }
+    }
+    return lowCost;
+}
+
+let highScore = printAndGetHighScore(scores);
+console.log("Bubble test: " + scores.length);
+console.log("Highest bubble score: " + highScore)
+
+let bestSolutions = getBestResults(scores, highScore);
+console.log("Solution with the highest score: " + bestSolutions);
+
+let mostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
+
+console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective");
