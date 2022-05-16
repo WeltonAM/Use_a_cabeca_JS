1e51833c154b22eaec75f9f026d6c786278a736e
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-05-16 15:47:22 -0300

    Create poderDoCerebro.js

---
+++ Programação_JS/07_tiposSerios/poderDoCerebro.js
@@ -0,0 +1,6 @@
+let text = "YOU SHOULD NEVER SHOUT WHEN TYPING";
+let presentableText = text.toLocaleLowerCase();
+if(presentableText.length > 0){
+    console.log(presentableText);
+}
+console.log(presentableText.length);
