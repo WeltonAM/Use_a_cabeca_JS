35e0d987048e705c1869890a9658472b9206ff08
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-05-16 17:45:00 -0300

    types

---
+++ Programação_JS/07_tiposSerios/lieDetecter.js
@@ -0,0 +1,45 @@
+function lieDetecterTest(){
+    let lies = 0;
+
+    let stolenDiamond = {};
+    if(stolenDiamond) {
+        console.log("You stole the diamond");
+        lies++;
+    }
+
+    let car = {
+        keysInPocket: null
+    };
+    if(car.keysInPocket) {
+        console.log("Uh oh, huess you stole the car!");
+        lies++;
+    }
+    if(car.emptyGasTank) {
+        console.log("You drove the car after you stole it!");
+        lies++;
+    }
+    
+    let foundYouAtTheCrimeScene = [];
+    if(foundYouAtTheCrimeScene){
+        console.log("A sure sine of guilt");
+        lies++;
+    }
+    if(foundYouAtTheCrimeScene[0]){
+        console.log("Caught with a stolen item!");
+        lies++;
+    }
+
+    let yourName = " ";
+    if(yourName) {
+        console.log("Guess you lied about your name");
+        lies++;
+    }
+
+    return lies;
+}
+
+let numberOfLies = lieDetecterTest();
+console.log(`You told ${numberOfLies} lies!`);
+if(numberOfLies >= 3) {
+    console.log("Guilty as charged!");
+}
