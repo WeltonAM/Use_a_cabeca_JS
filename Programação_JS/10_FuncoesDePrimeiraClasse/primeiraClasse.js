54005a28db389d5380669d04e7029bbe60d9b0ef
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-05-21 17:37:45 -0300

    Funções para funções

---
+++ Programação_JS/10_FuncoesDePrimeiraClasse/primeiraClasse.js
@@ -0,0 +1,46 @@
+let passengers = [
+    { name: "Jane Doloop", paid: true },
+    { name: "Dr. Evel", paid: true },
+    { name: "Sue Property", paid: false },
+    { name: "John Funcall", paid: true },
+]
+
+function processPassengers(passengers, testFunction) {
+    for(let i = 0; i < passengers.length; i++){
+        if(testFunction(passengers[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkNoFlyList(passenger) {
+    return (passenger.name === "Dr. Evel");
+}
+
+function checkNotPaid(passenger) {
+    return (!passenger.paid);
+}
+
+let allCanFly = processPassengers(passengers, checkNoFlyList);
+if(!allCanFly) {
+    console.log("The plane can't take off: we have a passenger on the no-fly-list.");
+}
+
+let allPaid = processPassengers(passengers, checkNotPaid);
+if(!allPaid) {
+    console.log("The plane can't take off: not everyone has paid.");
+}
+
+function printPassenger(passenger) {
+    let message = passenger.name;
+    if(passenger.paid === true) {
+        message = message + " has paid";
+    } else {
+        message = message + " has not paid";
+    }
+    
+    console.log(message);
+    return false;
+}
+processPassengers(passengers, printPassenger);
