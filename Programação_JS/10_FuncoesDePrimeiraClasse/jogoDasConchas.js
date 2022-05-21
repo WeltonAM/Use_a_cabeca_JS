3a80b944917cf4dfe860561a60d8b08e1d6faf36
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-05-21 16:19:13 -0300

    Create jogoDasConchas.js

---
+++ Programação_JS/10_FuncoesDePrimeiraClasse/jogoDasConchas.js
@@ -0,0 +1,19 @@
+let winner = function() { console.log("WINNER!") };
+let loser = function() { console.log("LOSER!") };
+
+winner();
+let a = winner;
+let b = loser;
+let c = loser;
+
+a();
+b();
+
+c = a;
+a = b;
+b = c;
+c = a;
+a = c;
+a = b;
+b = c;
+a();
