563106b49b0625a64f57dd67d2048c238d7e22e5
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-05-25 10:31:28 -0300

    Create construtorMundoReal.js

---
+++ Programação_JS/12_criandoObjetos/construtorMundoReal.js
@@ -0,0 +1,16 @@
+var now = new Date();
+var dateString = now.toString();
+var theYear = now.getFullYear();
+var theDayOfWeek = now.getDay();
+
+var birthday = new Date("May 1, 1983");
+
+console.log(now);
+console.log("---------");
+console.log(dateString);
+console.log("---------");
+console.log(theYear);
+console.log("---------");
+console.log(theDayOfWeek);
+console.log("---------");
+console.log(birthday);
