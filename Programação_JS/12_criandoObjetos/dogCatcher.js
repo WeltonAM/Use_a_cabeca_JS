f2a0b21e9f1df1fee3b4a1d8c1dfcc976a7aa9c4
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-05-25 10:06:54 -0300

    Identificar construtor

---
+++ Programação_JS/12_criandoObjetos/dogCatcher.js
@@ -0,0 +1,43 @@
+function dogCatcher(obj){
+    return (obj instanceof Dog);
+}
+
+function Cat(name, breed, weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+}
+
+var meow = new Cat("Meow", "Siamese", 10);
+var whiskers = new Cat("Whiskers", "Mixed", 12);
+
+var fido = {
+    name: "Fido",
+    breed: "Mixed",
+    weight: 38
+};
+
+function Dog(name, breed, weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+    this.bark = function() {
+        if(this.weight > 25) {
+            console.log(`${this.name} says WOOF!`);
+        } else {
+            console.log(`${this.name} says yip!`);
+        }
+    };
+}
+
+var fluffy = new Dog("Fluffy", "Poodle", 30);
+var spot = new Dog("Spot", "Chihuahua", 10);
+var dogs = [meow, whiskers, fido, fluffy, spot];
+
+for(let i = 0; i < dogs.length; i++){
+    if(dogCatcher(dogs[i])){
+        console.log(`${dogs[i].name} is a dog!`);
+    } else {
+        console.log(`It's not a dog!`);
+    }
+}
